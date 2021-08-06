e81e58b3e1a523800b20053d463eb7e757be9d59
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-08-06 21:29:07 +0300

    Code Challenge 3 done

--- java/array-binary-search/Main.java
+++ java/array-binary-search/Main.java
@@ -2,33 +2,51 @@ public class Main{
 
     public static void main(String[] args) {
         int [] testarr = {4, 8, 15, 16, 23, 42};
-        int num=BinarySearch(testarr,15);
+//      System.out.println("test= "+());
+        int num=BinarySearch(testarr,16);
         System.out.println(num);
+
 
     }
 
      public static int BinarySearch(int[]arr,int key){
-        int right=arr.length-1;
-        int left =0;
-        int middle;
-        int result=-1;
-        int count=0;
-        while(left<=right){
-            count++;
-        middle=(left+right)/2;
-        if(arr[middle]<key){
-            left=middle+1;
-        }else if(arr[middle]>key){
-            right=middle-1;
-        }else {
-            return middle;
-        }
-        return result;
-        }
-        result=count;
-
-
-        return  result;
+       int right=arr.length-1;
+       int left =0;
+       int result=-1;
+       while(left<=right){
+         int middle=(left+right)/2;
+         if(arr[middle]==key){
+           result=middle;
+           return  result;
+         }else if(arr[middle]>key){
+           right=middle-1;
+         }else {
+           left=middle+1;
+         }
+       }
+       return  result;
     }
 
 }
+
+
+
+//  int right=arr.length-1;
+//  int left =0;
+//  int result=-1;
+//  int count=0;
+//       while(left<=right){
+//         count++;
+//         int middle=(left+right)/2;
+//         if(arr[middle]<key){
+//  left=middle+1;
+//  }else if(arr[middle]>key){
+//  right=middle-1;
+//  }else {
+//  return middle;
+//  }
+//  return result;
+//  }
+//  result=count;
+//
+//  return  result;
